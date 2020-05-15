3ae48628b965a476f54b6fb5ed595a7fa5f5cb48
Gabriel <gabriel.developer@outlook.com>
2020-05-14 22:07:39 -0300

    trando do caso em que a busca falhou

--- src/usuarios/estrategias-autenticacao.js
+++ src/usuarios/estrategias-autenticacao.js
@@ -1,9 +1,19 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const Usuario = require('./usuarios-modelo');
 passport.use(
     new LocalStrategy({
         usernameField: 'email',
-        passwordField: 'senha'
+        passwordField: 'senha',
+        session: false
+    }, async (email, senha, done) => {
+        try {
+            const usuario = await Usuario.buscaPorEmail(email);
+        } catch (erro) {
+            done(erro);
+        }
+
+        
     })
 )
