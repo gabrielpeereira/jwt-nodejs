6264b41e6e0533499a3be89c71d2184b894b20ca
Gabriel <gabriel.developer@outlook.com>
2020-05-14 23:07:33 -0300

    inserindo a verificação de usuario

--- src/usuarios/estrategias-autenticacao.js
+++ src/usuarios/estrategias-autenticacao.js
@@ -2,6 +2,23 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError } = require('../erros');
+const bcrypt = require('bcrypt');
+
+function verificaUsuario(usuario) {
+    if(!usuario) {
+        throw new InvalidArgumentError('Não existe usuário com esse e-mail');
+    }
+}
+
+async function verificaSenha(senha, senhaHash) {
+    const senhaValida = await bcrypt.compare(senha, senhaHash);
+
+    if(!senha) {
+        throw new InvalidArgumentError('e-mail ou senha inválidos');
+    }
+}
+
 passport.use(
     new LocalStrategy({
         usernameField: 'email',
@@ -10,6 +27,11 @@ passport.use(
     }, async (email, senha, done) => {
         try {
             const usuario = await Usuario.buscaPorEmail(email);
+            verificaUsuario(usuario);
+            verificaSenha(senha, usuario.senhaHash);
+
+            done(null, usuario);
+
         } catch (erro) {
             done(erro);
         }
